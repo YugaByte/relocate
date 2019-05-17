fc283335ac221ee07f668ff5581cf7030a83e2b1
Mikhail Bautin <mbautin@users.noreply.github.com>
2019-05-16 23:15:01 -0700

    Fix tests

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .context import relocate
+from .context import ypack
 
 import unittest
 
@@ -9,7 +9,7 @@ class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
     def test_thoughts(self):
-        self.assertIsNone(relocate.hmm())
+        self.assertIsNone(ypack.hmm())
 
 
 if __name__ == '__main__':
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .context import relocate
+from .context import ypack
 
 import unittest
 
